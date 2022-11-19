794942f4fd9ead6ed2a574d54430c5fa25f937b0
JPhillip018 <jphillip018@gmail.com>
2022-11-19 12:19:06 -0500

    created routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 
 
+
 function App() {
-  return <Nav/>
-    
+  return 
+    <>
+  {/* // <React.Fragment> */}
+    <Nav/>
+    <Routes>
+      <Route path='/auth' element={<Auth/>}></Route>
+      <Route path='/posts' element={<Posts/>}></Route>
+      <Route path='/myPosts' element={<UserPosts/>}></Route>
+      <Route path='/myPosts/:id' element={<PostDetails/>}></Route>
+      <Route path='/posts/create' element={<createPost/>}></Route>
+    </Routes>
+  </>
 }
 
 export default App;
