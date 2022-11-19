781c70dd7312301977db99bf569bad9682b398f1
JPhillip018 <jphillip018@gmail.com>
2022-11-19 14:14:34 -0500

    linked tabs and buttons to theirpaths

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React, {useState}from 'react'
 import { AppBar, Button, Toolbar, Typography, Box, Tab, Tabs } from '@mui/material'
 import '../App.css'
-
+import {Link} from 'react-router-dom'
 
  const Nav = () => {
   const [value, setValue] = useState();
@@ -11,16 +11,16 @@ import '../App.css'
         <Typography variant='h4'> Explorer </Typography>
         <Box class='Tabs'>
           <Tabs textColor='white' value={value} onChange={(e,val) => setValue(val)}>
-            <Tab label="About"></Tab>
-            <Tab label="All Posts"></Tab>
-            <Tab label="My Posts"></Tab>
-            <Tab label="Explore"></Tab>
+            <Tab LinkComponent={Link} to='/about' label="About"></Tab>
+            <Tab LinkComponent={Link} to='/posts' label="All Posts"></Tab>
+            <Tab LinkComponent={Link} to='/myPosts' label="My Posts"></Tab>
+            <Tab LinkComponent={Link} to='/explore' label="Explore"></Tab>
           </Tabs>
         </Box>
         <Box class='NavBox'>
-          <Button class='NavBtn'> Register </Button>
-          <Button class='NavBtn'> Login </Button>
-          <Button class='NavBtn'> Logout </Button>
+          <Button LinkComponent={Link} to='/auth' variant='contained' sx={{ margin: 1, borderRadius: 10 }}> Register </Button>
+          <Button LinkComponent={Link} to='/auth' variant='contained' sx={{ margin: 1, borderRadius: 10 }}> Login </Button>
+          <Button LinkComponent={Link} to='/auth' variant='contained' sx={{ margin: 1, borderRadius: 10 }}> Logout </Button>
         </Box>
       </Toolbar>
     </AppBar>
