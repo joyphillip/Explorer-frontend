ab286bed26fee720ba61942f63600edbb47cd247
JPhillip018 <jphillip018@gmail.com>
2022-11-21 19:22:22 -0500

    created userLogin function

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,6 +2,7 @@ import "../App.css"
 import { Button, Typography, TextField, Box } from '@mui/material'
 import React, { useState } from 'react'
 
+let baseURL = process.env.REACT_APP_BACKEND_URL
 
 export const Auth = () => {
   const [inputs, setInputs] = useState({
@@ -19,9 +20,29 @@ export const Auth = () => {
     }))
   }
 
+
+  const loginUser = (e) => {
+    e.preventDefault()
+    fetch(baseURL + '/user/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: inputs.email,
+        password: inputs.password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: "include"
+    }).then(res => res.json())
+    .then(resJson => {
+      console.log(resJson)
+    })
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs)
+    loginUser()
   }
 
   return (
@@ -46,7 +67,7 @@ export const Auth = () => {
         {isRegistered && <TextField name='name' onChange={handleChange} value={inputs.name} label='Name' margin="normal"/>}{" "}
           <TextField  name='email' onChange={handleChange} value={inputs.email} type={'email'} label='Email' margin="normal"/>
           <TextField name='password' onChange={handleChange} value={inputs.password} type={'password'} label='Password' margin="normal"/> 
-          <Button type='submit' sx={{borderRadius: 2, margin: 2}} variant='contained' color='success'> Register </Button>
+          <Button type='submit' sx={{borderRadius: 2, margin: 2}} variant='contained' color='success'> Enter </Button>
           
           <div>Already have an account?</div>
           <Button onClick={()=> setIsRegistered(!isRegistered)}> Login</Button>
