a178b9e5ea0da13b44f3d2f8c19187fdf1033b33
JPhillip018 <jphillip018@gmail.com>
2022-11-28 17:06:59 -0500

    created about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import { Avatar, Box, Typography } from '@mui/material'
 const About = () => {
+
   return (
-    <div>About</div>
+    <div>
+      <Box 
+        backgroundColor='white'
+        border="inset"
+        borderColor='black'
+        borderRadius={10}
+        padding={3}
+        margin='auto'
+        marginTop={4}
+        display='flex'
+        flexDirection={'column'}
+        width={'60%'}
+        >
+        <Typography
+          variant='h4'
+          textAlign={'center'}
+          padding={3}
+          > About
+          </Typography>
+
+          
+          <Typography
+          textAlign={'center'}>
+            Hello & Welcome to Explorer,
+            Here you can browse postcards from friends all over the world and even store your travel memories.
+            Have fun!
+          </Typography>
+        </Box>
+    </div>
   )
 }
 
